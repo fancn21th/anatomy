b8482520a068c333bb805bf12b8c555f016564ca
fancn21th <fancn21th@gmail.com>
2024-04-03 14:46:46 +0800

    📦 NEW: 动画

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,44 @@
 "use client";
-
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 import { Theme } from "@radix-ui/themes";
 import { Card } from "@fancn21th/molecules-organisms-bones";
-import "@radix-ui/themes/styles.css";
 
 import styles from "./page.module.css";
 
 export default function Home() {
+  const container = useRef<HTMLElement | null>(null);
+
+  useGSAP(
+    () => {
+      container.current &&
+        gsap.from(".card", {
+          scale: 0,
+          duration: 1,
+          repeat: -1,
+          ease: "power1.inOut",
+          yoyo: true,
+          stagger: {
+            each: 0.2,
+          },
+        });
+    },
+    { scope: container }
+  );
+
   return (
     <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
-      <main className={styles.main}>
-        <Card />
+      <main className={styles.main} ref={container}>
+        <div className="card">
+          <Card />
+        </div>
+        <div className="card">
+          <Card />
+        </div>
+        <div className="card">
+          <Card />
+        </div>
       </main>
     </Theme>
   );
