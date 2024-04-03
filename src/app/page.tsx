bd6ecb63fde77cfdf6514f987aedcad791136ed1
fancn21th <fancn21th@gmail.com>
2024-04-03 13:46:16 +0800

    📦 NEW: 使用 card 组件

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Theme } from "@radix-ui/themes";
-import { Test } from "@fancn21th/molecules-organisms-bones";
+import { Card } from "@fancn21th/molecules-organisms-bones";
 import "@radix-ui/themes/styles.css";
 
 import styles from "./page.module.css";
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Theme accentColor="crimson" grayColor="sand" radius="large" scaling="95%">
       <main className={styles.main}>
-        <Test />
+        <Card />
       </main>
     </Theme>
   );
